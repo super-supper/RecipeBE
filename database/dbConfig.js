119139535704158c13968b959c058eaf389a5ea2
HeftyB <Ashields0923@gmail.com>
2021-03-09 20:56:11 -0500

    heroku migration

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require("knex");
-const config = require("./knexfile.js");
-const environment = process.env.DB_ENV || "development";
+const config = require("../knexfile.js");
+const environment = process.env.DB_ENV;
 const db = knex(config[environment])
 
 module.exports = db;
